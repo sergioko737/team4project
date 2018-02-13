8234fdf6da3d18f58f3bd24e778d5a3ef9aa9c20
Sergejs Kostjucenko <sergejs.kostjucenko@gmail.com>
2018-02-13 09:45:31 +0200

    Initial commit

---
+++ core/src/com/jbcteam4/androidgame/states/MenuState.java
@@ -0,0 +1,54 @@
+package com.jbcteam4.androidgame.states;
+
+        import com.badlogic.gdx.Gdx;
+        import com.badlogic.gdx.graphics.Texture;
+        import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+        import com.jbcteam4.androidgame.FlappyDemo;
+
+
+public class MenuState extends State {
+
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        camera.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void handleInput() {
+        if(Gdx.input.justTouched()){
+            gsm.set(new PlayState(gsm));
+        }
+
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(camera.combined);
+        sb.begin();
+        sb.draw(background, 0, 0);
+        sb.draw(playBtn, camera.position.x - playBtn.getWidth() / 2, camera.position.y);
+        sb.end();
+
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
+
+        System.out.println("MenuState Disposed");
+
+    }
+}
---
+++ desktop/src/com/jbcteam4/androidgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jbcteam4.androidgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jbcteam4.androidgame.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
